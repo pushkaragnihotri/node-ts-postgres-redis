b55a7c596ae1ee38338ad8937d709ca398243ede
Pushkar Sharma <pushkar@convosight.com>
2020-08-31 00:07:08 +0530

    create a project structure with Express.js

---
+++ src/middleware/common.ts
@@ -0,0 +1,20 @@
+import express, { Router } from 'express'
+import cors from 'cors'
+import compression from 'compression'
+
+export const handleCors = (router: Router) =>
+	router.use(
+		cors({
+			credentials: true,
+			origin: true,
+		})
+	)
+
+export const handleBodyRequestParsing = (router: Router) => {
+	router.use(express.urlencoded({ extended: true }))
+	router.use(express.json())
+}
+
+export const handleCompression = (router: Router) => {
+	router.use(compression())
+}
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-let i: number = 0
-const printer = () => (console.log(i), i++)
-setInterval(() => printer(), 1000)
+import express from 'express'
+import http from 'http'
+import { applyMiddleware, applyRoutes } from './utils'
+import middleware from './middleware'
+import routes from './services'
+console.log(middleware)
+
+const router: express.Application = express()
+applyMiddleware(middleware, router)
+applyRoutes(routes, router)
+
+const { PORT = 3000 } = process.env
+const server = http.createServer(router)
+
+server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`))
